1b7909359e500574f3ffbd934f45307c545ebfcb
ShivamKumarJha <jha.shivam3@gmail.com>
2019-06-09 11:01:41 +0530

    system_vendor_prop.sh: Introduce new properties maker script
     * This script keeps system & optional vendor properties in separate files unlike other other script.
     * Output: system.prop & vendor_prop.mk
     * No properites filtering here
    
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ tools/system_vendor_prop.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2019 Shivam Kumar Jha <jha.shivam3@gmail.com>
+#
+# Helper functions
+
+# Store project path
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
+
+# Text format
+source $PROJECT_DIR/tools/colors.sh
+
+# Create $PROJECT_DIR/working directory if it does not exist
+if [ ! -d $PROJECT_DIR/working ]; then
+	mkdir -p $PROJECT_DIR/working
+fi
+
+# clean up
+rm -rf $PROJECT_DIR/working/*
+
+# Exit if no arguements
+if [ -z "$1" ] ; then
+	echo -e "${bold}${red}Supply sytem &/ vendor build.prop as arguements!${nocol}"
+	exit
+fi
+
+# Get files via either cp or wget
+if echo "$1" | grep "https" ; then
+	wget -O $PROJECT_DIR/working/system_working.prop $1
+else
+	cp -a $1 $PROJECT_DIR/working/system_working.prop
+fi
+if [ ! -z "$2" ] ; then
+	if echo "$2" | grep "https" ; then
+		wget -O $PROJECT_DIR/working/vendor_working.prop $2
+	else
+		cp -a $2 $PROJECT_DIR/working/vendor_working.prop
+	fi
+fi
+
+# system.prop
+TSTART=$(grep -nr "# end build properties" $PROJECT_DIR/working/system_working.prop | sed "s|:.*||g")
+TSTART=$((TSTART+1))
+TEND=$(grep -nr "# ADDITIONAL_BUILD_PROPERTIES" $PROJECT_DIR/working/system_working.prop | sed "s|:.*||g")
+TEND=$((TEND-1))
+sed -n "${TSTART},${TEND}p" $PROJECT_DIR/working/system_working.prop > $PROJECT_DIR/working/system.prop
+
+# put some properties
+printf "\n# Additional properties\n" >> $PROJECT_DIR/working/system.prop
+echo "ro.am.reschedule_service=true" >> $PROJECT_DIR/working/system.prop
+echo "ro.sys.fw.use_trim_settings=true" >> $PROJECT_DIR/working/system.prop
+
+# Lineage vendor security patch support
+if [ $(grep "ro.build.version.release=" $PROJECT_DIR/working/system_working.prop | sed "s|ro.build.version.release=||g" | head -c 1) -lt 9 ]; then
+	grep "ro.build.version.security_patch=" $PROJECT_DIR/working/system_working.prop | sed "s|ro.build.version.security_patch|ro.lineage.build.vendor_security_patch|g" >> $PROJECT_DIR/working/system.prop
+fi
+
+# Default LCD density
+if ! grep -q "ro.sf.lcd_density=" $PROJECT_DIR/working/system.prop; then
+	printf "\n# LCD density\n" >> $PROJECT_DIR/working/system.prop
+	echo "ro.sf.lcd_density=440" >> $PROJECT_DIR/working/system.prop
+fi
+
+# vendor.prop
+if [ ! -z "$2" ] ; then
+	TSTART=$(grep -nr "# end build properties" $PROJECT_DIR/working/vendor_working.prop | sed "s|:.*||g")
+	echo "###ENDDD" >> $PROJECT_DIR/working/vendor_working.prop
+	TEND=$(grep -nr "###ENDDD" $PROJECT_DIR/working/vendor_working.prop | sed "s|:.*||g")
+	sed -n "${TSTART},${TEND}p" $PROJECT_DIR/working/vendor_working.prop | sort | sed "s|#.*||g" | sed '/^[[:space:]]*$/d' > $PROJECT_DIR/working/vendor_new.prop
+	sed -i -e 's/^/    /' $PROJECT_DIR/working/vendor_new.prop
+	sed -i '1 i\PRODUCT_PROPERTY_OVERRIDES += ' $PROJECT_DIR/working/vendor_new.prop
+	awk 'NF{print $0 " \\"}' $PROJECT_DIR/working/vendor_new.prop > $PROJECT_DIR/working/vendor_prop.mk
+fi
+
+# cleanup
+rm -rf $PROJECT_DIR/working/system_working.prop $PROJECT_DIR/working/vendor_new.prop $PROJECT_DIR/working/vendor_working.prop
