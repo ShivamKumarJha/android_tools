74a428f108ed58986e54bac8b2a69bc9d15521c4
ShivamKumarJha <jha.shivam3@gmail.com>
2019-06-16 10:55:32 +0530

    dependencies.sh: Update password handling & do shallow clone
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- tools/dependencies.sh
+++ tools/dependencies.sh
@@ -9,22 +9,20 @@
 # Store project path
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
 
-# Text format
-source $PROJECT_DIR/tools/colors.sh
-
-# Exit if no arguements
-if [ -z "$1" ] ; then
-	echo -e "${bold}${red}Supply password as arguement!${nocol}"
-	exit
+# Password
+if [ -z "$1" ]; then
+	read -p "Enter user password: " user_password
+else
+	user_password=$1
 fi
 
 # Clone repo's
 for toolsdir in "extract_android_ota_payload" "mkbootimg_tools" "sdat2img"; do
 	if [ ! -d "$PROJECT_DIR/tools/$toolsdir" ]; then
-		git clone -q https://gitlab.com/ShivamKumarJha/$toolsdir $PROJECT_DIR/tools/$toolsdir
+		git clone -q https://gitlab.com/ShivamKumarJha/$toolsdir $PROJECT_DIR/tools/$toolsdir --depth 1
 		chmod +x $PROJECT_DIR/tools/$toolsdir/*
 	fi
 done
 
 # Install some packages
-echo "$1" | sudo -S apt-get install -y android-tools-fsutils brotli git
+echo "$user_password" | sudo -S apt-get install -y android-tools-fsutils brotli git
