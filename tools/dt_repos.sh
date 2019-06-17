59507e41b67c96ed0e7e86aa0e0a344b8a9f7d39
ShivamKumarJha <jha.shivam3@gmail.com>
2019-06-17 16:42:41 +0530

    dt_repos.sh: Introduce script to create device tree's on GitHub
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ tools/dt_repos.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Copyright (C) 2019 Shivam Kumar Jha <jha.shivam3@gmail.com>
+#
+# Helper functions
+
+# Store project path
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null && pwd )"
+
+# Common stuff
+source $PROJECT_DIR/tools/common_script.sh
+
+# Exit if no arguements
+if [ -z "$1" ] ; then
+	echo -e "${bold}${red}Supply dir's as arguements!${nocol}"
+	exit
+fi
+
+# Exit if missing token's
+if [ -z "$GIT_TOKEN" ] || [ -z "$TG_API" ]; then
+	echo -e "${bold}${cyan}Missing GitHub or telegram token. Exiting.${nocol}"
+	exit
+fi
+
+# o/p
+for var in "$@"; do
+	source $PROJECT_DIR/tools/rom_vars.sh "$var" > /dev/null 2>&1
+	DT_REPO=$(echo device_$BRAND\_$DEVICE | sort -u | head -n 1 )
+	KT_REPO=$(echo kernel_$BRAND\_$DEVICE | sort -u | head -n 1 )
+	VT_REPO=$(echo vendor_$BRAND\_$DEVICE | sort -u | head -n 1 )
+	if [ -z "$MODEL" ]; then
+		DT_REPO_DESC=$(echo "Device-tree-for-$DEVICE" | sort -u | head -n 1 )
+		KT_REPO_DESC=$(echo "Kernel-tree-for-$DEVICE" | sort -u | head -n 1 )
+		VT_REPO_DESC=$(echo "Vendor-tree-for-$DEVICE" | sort -u | head -n 1 )
+	else
+		DT_REPO_DESC=$(echo "Device-tree-for-$MODEL" | tr ' ' '-' | sort -u | head -n 1 )
+		KT_REPO_DESC=$(echo "Kernel-tree-for-$MODEL" | tr ' ' '-' | sort -u | head -n 1 )
+		VT_REPO_DESC=$(echo "Vendor-tree-for-$MODEL" | tr ' ' '-' | sort -u | head -n 1 )
+	fi
+	# Create repository in GitHub
+	printf "${bold}${cyan}Creating\nhttps://github.com/ShivamKumarJha/$DT_REPO\nhttps://github.com/ShivamKumarJha/$KT_REPO\nhttps://github.com/ShivamKumarJha/$VT_REPO\n${nocol}"
+	curl https://api.github.com/user/repos\?access_token=$GIT_TOKEN -d '{"name":"'${DT_REPO}'","description":"'${DT_REPO_DESC}'","private": true,"has_issues": true,"has_projects": false,"has_wiki": true}' > /dev/null 2>&1
+	curl https://api.github.com/user/repos\?access_token=$GIT_TOKEN -d '{"name":"'${KT_REPO}'","description":"'${KT_REPO_DESC}'","private": true,"has_issues": true,"has_projects": false,"has_wiki": true}' > /dev/null 2>&1
+	curl https://api.github.com/user/repos\?access_token=$GIT_TOKEN -d '{"name":"'${VT_REPO}'","description":"'${VT_REPO_DESC}'","private": true,"has_issues": true,"has_projects": false,"has_wiki": true}' > /dev/null 2>&1
+done
