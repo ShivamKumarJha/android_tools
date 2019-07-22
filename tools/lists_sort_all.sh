db21765ddbdf9c62e30d57dfab29e0ab0c6e389e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-22 22:56:40 +0530

    lists: proprietary: Add Script to add new platform & update lists
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- tools/lists_sort_all.sh
+++ tools/lists_sort_all.sh
@@ -10,7 +10,6 @@
 LISTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/lists/" >/dev/null && pwd )"
 
 tools_lists=`find $LISTS_DIR/ -type f -printf '%P\n' | sort | grep -v "overlays/comments/"`
-for list in $tools_lists ;
-do
+for list in $tools_lists; do
 	sort -u -o "$LISTS_DIR/$list" "$LISTS_DIR/$list"
 done
