291ac07eb45b5dbe60454a9ded92841039020dac
ShivamKumarJha <jha.shivam3@gmail.com>
2019-07-26 18:01:49 +0530

    sony_rom.sh: Add tz assertion
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- tools/sony_rom.sh
+++ tools/sony_rom.sh
@@ -45,8 +45,8 @@ for var in "$@"; do
 	unzip $URL -d $PROJECT_DIR/working
 	# Extract sin
 	[[ -e $PROJECT_DIR/working/userdata.sin ]] && rm -rf $PROJECT_DIR/working/userdata.sin
-	mono $PROJECT_DIR/tools/prebuilt/UnSIN.exe -d $PROJECT_DIR/working
-	rm -rf $PROJECT_DIR/working/*.sin
+	mono $PROJECT_DIR/tools/prebuilt/UnSIN.exe -dr $PROJECT_DIR/working
+	find $PROJECT_DIR/working/ -name "*.sin" -type f -exec rm -rf {} \;
 	[[ -d $PROJECT_DIR/dumps/$UNZIP_DIR/ ]] && rm -rf $PROJECT_DIR/dumps/$UNZIP_DIR/
 	# mounting
 	ext4_list=`find $PROJECT_DIR/working -type f \( -name "*ext4*" -o -name "modem.img" -o -name "bluetooth.img" \) -printf '%P\n' | sort`
@@ -72,6 +72,14 @@ for var in "$@"; do
 	find $PROJECT_DIR/dumps/$UNZIP_DIR/modem -type f -exec strings {} \; | grep "Time_Stamp\": \"" | tr -d ' ' | cut -c 15- | sed 's/.$//' | sed -e 's/^/require version-modem=/' >> $PROJECT_DIR/dumps/$UNZIP_DIR/board-info.txt
 	if [ -e $PROJECT_DIR/dumps/$UNZIP_DIR/vendor/build.prop ]; then
 		strings $PROJECT_DIR/dumps/$UNZIP_DIR/vendor/build.prop | grep "ro.vendor.build.date.utc" | sed "s|ro.vendor.build.date.utc|require version-vendor|g" >> $PROJECT_DIR/dumps/$UNZIP_DIR/board-info.txt
+	fi
+	if [ -d $PROJECT_DIR/working/boot/ ]; then
+		boot_list=`find $PROJECT_DIR/working/boot/ -type f -printf '%P\n' | sort`
+		for boot_file in $boot_list; do
+			DIR_NAME=$(echo $boot_file | cut -d . -f1)
+			7z x $PROJECT_DIR/working/boot/$boot_file -y -o$PROJECT_DIR/working/boot/$DIR_NAME/ 2>/dev/null >> $PROJECT_DIR/working/boot/zip.log
+		done
+		find $PROJECT_DIR/working/boot/ -name "tz*" -type f -exec strings {} \; | grep "QC_IMAGE_VERSION_STRING" | sed "s|QC_IMAGE_VERSION_STRING|require version-trustzone|g" >> $PROJECT_DIR/dumps/$UNZIP_DIR/board-info.txt
 	fi
 	sort -u -o $PROJECT_DIR/dumps/$UNZIP_DIR/board-info.txt $PROJECT_DIR/dumps/$UNZIP_DIR/board-info.txt
 	# boot.img operations
