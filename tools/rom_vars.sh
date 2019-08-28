33de448b2a142d0de6fe927e33827c139d531aff
ShivamKumarJha <jha.shivam3@gmail.com>
2019-08-28 00:59:54 +0000

    rom_vars.sh: update for google
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- tools/rom_vars.sh
+++ tools/rom_vars.sh
@@ -46,6 +46,8 @@ for var in "$@"; do
     BRAND=${BRAND_TEMP,,}
     if grep -q "ro.vivo.product.release.name" "$CAT_FILE"; then
         DEVICE=$( cat "$CAT_FILE" | grep "ro.vivo.product.release.name=" | sed "s|.*=||g" | head -n 1 )
+    elif grep -q "ro.build.product=" "$CAT_FILE"; then
+        DEVICE=$( cat "$CAT_FILE" | grep "ro.build.product=" | sed "s|.*=||g" | head -n 1 )
     elif grep -q "ro.product.system.name" "$CAT_FILE"; then
         DEVICE=$( cat "$CAT_FILE" | grep "ro.product.system.name=" | sed "s|.*=||g" | head -n 1 )
     else
