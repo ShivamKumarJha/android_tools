c4e2f7299ed7b0aa67ad14778525a7db8edc9ce0
ShivamKumarJha <jha.shivam3@gmail.com>
2020-01-07 20:19:30 +0530

    rebase_kernel.sh: improve nested zip detection
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- tools/rebase_kernel.sh
+++ tools/rebase_kernel.sh
@@ -36,13 +36,13 @@ UNZIP_DIR=${FILE/.$EXTENSION/}
 echo "Extracting file"
 mkdir -p ${PROJECT_DIR}/kernels/${UNZIP_DIR}
 7z x ${PROJECT_DIR}/input/${FILE} -y -o${PROJECT_DIR}/kernels/${UNZIP_DIR} > /dev/null 2>&1
+KERNEL_DIR="$(dirname "$(find ${PROJECT_DIR}/kernels/${UNZIP_DIR} -type f -name "AndroidKernel.mk" | head -1)")"
+[[ ! -e ${KERNEL_DIR}/Makefile ]] && KERNEL_DIR="$(dirname "$(find ${PROJECT_DIR}/kernels/${UNZIP_DIR} -type f -name "build.config.goldfish.arm64" | head -1)")"
 NEST="$( find ${PROJECT_DIR}/kernels/${UNZIP_DIR} -type f -size +50M \( -name "*.rar*" -o -name "*.zip*" -o -name "*.tar*" \) -printf '%P\n' | head -1)"
-if [ ! -z ${NEST} ]; then
+if [ ! -z ${NEST} ] && [[ ! -e ${KERNEL_DIR}/Makefile ]]; then
     bash ${PROJECT_DIR}/tools/rebase_kernel.sh ${PROJECT_DIR}/kernels/${UNZIP_DIR}/${NEST} ${2} ${3}
     exit
 fi
-KERNEL_DIR="$(dirname "$(find ${PROJECT_DIR}/kernels/${UNZIP_DIR} -type f -name "AndroidKernel.mk" | head -1)")"
-[[ ! -e ${KERNEL_DIR}/Makefile ]] && KERNEL_DIR="$(dirname "$(find ${PROJECT_DIR}/kernels/${UNZIP_DIR} -type f -name "build.config.goldfish.arm64" | head -1)")"
 cd ${KERNEL_DIR}
 
 # Find kernel version
