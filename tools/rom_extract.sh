152bdaad52dfafcc7cdc045e54e10f718d3bb22c
Akhil Narang <me@akhilnarang.dev>
2020-03-30 00:10:57 +0530

    rom_extract.sh: add some more partitions found in pixel r preview
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- tools/rom_extract.sh
+++ tools/rom_extract.sh
@@ -43,7 +43,7 @@ for var in "$@"; do
     FILE=${URL##*/}
     EXTENSION=${URL##*.}
     UNZIP_DIR=${FILE/.$EXTENSION/}
-    PARTITIONS="system vendor cust odm oem factory product modem xrom systemex oppo_product preload_common"
+    PARTITIONS="system vendor cust odm oem factory product modem xrom systemex oppo_product preload_common system_ext system_other"
     [[ -d $PROJECT_DIR/dumps/$UNZIP_DIR/ ]] && rm -rf $PROJECT_DIR/dumps/$UNZIP_DIR/
 
     if [ -d "$var" ] ; then
