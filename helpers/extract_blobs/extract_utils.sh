e6c9a6b44f8fa009bbad458980ab930e56e9c1c2
dianlujitao <dianlujitao@lineageos.org>
2020-04-21 23:01:13 +0800

    extract_utils: Handle Brotli compressed block image
    Change-Id: I1b3ef34ebba9b43fd684c6abaa1dbc77c8786da7

--- helpers/extract_blobs/extract_utils.sh
+++ helpers/extract_blobs/extract_utils.sh
@@ -1478,6 +1478,11 @@ function extract() {
             for PARTITION in "system" "odm" "product" "vendor"
             do
                 # If OTA is block based, extract it.
+                if [ -a "$DUMPDIR"/"$PARTITION".new.dat.br ]; then
+                    echo "Converting "$PARTITION".new.dat.br to "$PARTITION".new.dat"
+                    brotli -d "$DUMPDIR"/"$PARTITION".new.dat.br
+                    rm "$DUMPDIR"/"$PARTITION".new.dat.br
+                fi
                 if [ -a "$DUMPDIR"/"$PARTITION".new.dat ]; then
                     echo "Converting "$PARTITION".new.dat to "$PARTITION".img"
                     python3 "$LINEAGE_ROOT"/helpers/extract_blobs/sdat2img.py "$DUMPDIR"/"$PARTITION".transfer.list "$DUMPDIR"/"$PARTITION".new.dat "$DUMPDIR"/"$PARTITION".img 2>&1
