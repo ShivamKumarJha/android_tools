8d7caade085ca74e50c2784d925e30924fbe2fa6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-12-16 12:09:19 +0100

    proprietary-files: Move Media section to get_hardware_module function

--- tools/proprietary-files.sh
+++ tools/proprietary-files.sh
@@ -332,8 +332,12 @@ search_blobs | grep "vendor/" | grep -iE "liblisten" | add_to_section Listen
 search_blobs | grep "vendor/" | grep -iE "mlshal" | add_to_section Machine-Learning
 
 # Media
-search_blobs | grep -iE "lib/|lib64/" | grep -iE "extractors/libmmparser.so|libFileMux.so|libOmxMux.so|libmmosal.so|ibmmparser_lite.so|libmmrtpdecoder.so|libmmrtpencoder.so|vendor.qti.hardware.vpp@" | add_to_section Media
-search_blobs | grep "vendor/" | grep -iE "vendor.qti.hardware.vpp|libvpp" | add_to_section Media
+media_targets=(
+    "vendor.qti.hardware.vpp"
+)
+search_blobs | get_hardware_module "${media_targets[@]}" | add_to_section Media
+search_blobs | grep -iE "lib/|lib64/" | grep -iE "extractors/libmmparser.so|libFileMux.so|libOmxMux.so|libmmosal.so|ibmmparser_lite.so|libmmrtpdecoder.so|libmmrtpencoder.so" | add_to_section Media
+search_blobs | grep "vendor/" | grep -iE "libvpp" | add_to_section Media
 
 # Mediatek
 search_blobs | grep "vendor/" | grep -iE "mediatek|libmtk" | add_to_section Mediatek
