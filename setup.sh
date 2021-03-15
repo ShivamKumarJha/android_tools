e7863086bb6846bbb64a3b952afd5c3423c0d906
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-16 01:23:49 +0530

    setup: remove file-roller
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ fi
 
 # Install some packages
 if [[ -e "/usr/bin/apt-get" ]]; then
-    apt-get install -y aria2 arj brotli cabextract cmake device-tree-compiler file-roller g++ git liblz4-tool liblzma-dev libtinyxml2-dev mpack p7zip-full p7zip-rar python3-pip rar sharutils unace unrar unzip uudeview zip
+    apt-get install -y aria2 arj brotli cabextract cmake device-tree-compiler g++ git liblz4-tool liblzma-dev libtinyxml2-dev mpack p7zip-full p7zip-rar python3-pip rar sharutils unace unrar unzip uudeview zip
 elif [[ -e "/usr/bin/pacman" ]]; then
-    pacman -Syu --noconfirm android-tools aria2 arj brotli cabextract cmake dtc file-roller gcc git lz4 xz tinyxml2 p7zip python2-pip python-pip unrar sharutils unace zip unzip uudeview zip
+    pacman -Syu --noconfirm android-tools aria2 arj brotli cabextract cmake dtc gcc git lz4 xz tinyxml2 p7zip python2-pip python-pip unrar sharutils unace zip unzip uudeview zip
 fi
 pip3 install backports.lzma docopt protobuf pycrypto zstandard
